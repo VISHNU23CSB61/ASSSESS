22f214e83472f002148d0cfe1637c7d6e066ad18
surya <suryafranky007@gmail.com>
2025-02-28 06:20:44 +0000

    1st commit

---
+++ calc.java
@@ -0,0 +1,17 @@
+public class MathOperations {
+     	public static int add(int a, int b) {
+	     	return a + b;
+   	}
+       	public static int subtract(int a, int b) {
+	                                      return a - b;
+	                                            }
+	                                                 
+	                                                    public static void main(String[] args) {
+	                                                             int num1 = 5;
+	                                                                     int num2 = 10;
+	                                                                             
+	                                                                                     System.out.println("Addition: " + add(num1, num2));
+	                                                                                             System.out.println("Subtraction: " + subtract(num1, num2));
+	                                                                                                 }
+	                                                                                                 }
+	    
